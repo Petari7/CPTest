bc674b6de3e7f3afe439fb53e39700eae1db7eef
Petari7 <ivic.petar77@gmail.com>
2020-08-18 10:56:22 +0200

    Minor bug fixed [] removed

--- CPTest/AppDelegate.swift
+++ CPTest/AppDelegate.swift
@@ -32,7 +32,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     
     func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-        [CleverPush .didRegister(forRemoteNotifications: application, deviceToken: deviceToken)]
+        CleverPush.didRegister(forRemoteNotifications: application, deviceToken: deviceToken)
     }
     
     
